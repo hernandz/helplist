386b150354252c8867c79fe963a820b9e8879895
hernandz <hernandz@stanford.edu>
2015-12-04 14:07:16 -0800

    post gres fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151204041758) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
